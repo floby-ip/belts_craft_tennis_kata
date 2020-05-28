8ddabc8ac703befdf0b5d625fe82def1b63c3ee0
fberge <fberge@ippon.fr>
2020-05-28 16:38:03 +0200

    refactoring simple tennis 1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,6 +1,16 @@
 
 public class TennisGame1 implements TennisGame {
-    
+
+    private static final String STR_ADVANTAGE = "Advantage ";
+    private static final String STR_WIN = "Win for ";
+    private static final String STR_0 = "Love";
+    private static final String STR_15 = "Fifteen";
+    private static final String STR_30 = "Thirty";
+    private static final String STR_40 = "Forty";
+    private static final String STR_DEUCE = "Deuce";
+    private static final String STR_ALL = "All";
+    private static final String STR_SCORE_SEP = "-";
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -11,66 +21,126 @@ public class TennisGame1 implements TennisGame {
         this.player2Name = player2Name;
     }
 
+    /**
+     * Incrementer le score du joueur en parametre
+     *
+     * @param playerName le nom du joueur qui vient de mettre le point
+     */
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals(this.player1Name))
             m_score1 += 1;
         else
             m_score2 += 1;
     }
 
+    /**
+     * Traduire le score "numerique" en score "tennis"
+     *
+     * @return le score en String
+     */
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        // Egalité : gestion du A ou Deuce
+        if (m_score1 == m_score2) {
+            return getStrForEquality();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+
+        // > 40 : Gestion Avantage ou victoire
+        if (m_score1 >= 4 || m_score2 >= 4) {
+            return getStrForScoreOver40();
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+
+        // < 40 et non egal : score classique
+        return getStrForScoreUnder40NoEquality();
+    }
+
+    /**
+     * Traduit le score dans le cas où il n'y a pas égalité et les scores sont < 40
+     *
+     * @return
+     */
+    private String getStrForScoreUnder40NoEquality() {
+        return getScoreStr(getStrForPoints(m_score1), getStrForPoints(m_score2));
+    }
+
+    /**
+     * Traduit le score si un des deux score est > 40 et qu'il n'y a pas égalité
+     *
+     * @return
+     */
+    private String getStrForScoreOver40() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) { // Avantage player1
+            return getAdvantageStr(player1Name);
         }
-        return score;
+
+        if (minusResult == -1) { // Avantage player2
+            return getAdvantageStr(player2Name);
+        }
+
+        if (minusResult >= 2) { // Victoire player1
+            return getWinStr(player1Name);
+        }
+        // Victoire player2
+        return getWinStr(player2Name);
+    }
+
+    /**
+     * Traduit le score si il y a egalité
+     * Si < 3 : score1-All , sinon DEUCE
+     * @return Le score en String
+     */
+    private String getStrForEquality() {
+        return m_score1 < 3 ? getScoreStr(getStrForPoints(m_score1), STR_ALL) : STR_DEUCE;
+    }
+
+    /**
+     * Concatenation de Advantage et le nom du joueur
+     *
+     * @param playerName
+     * @return la string Advantage Joueur
+     */
+    private String getAdvantageStr(String playerName) {
+        return new StringBuilder(STR_ADVANTAGE).append(playerName).toString();
+    }
+
+    /**
+     * Concatenation de Win for est le nom du joueur
+     *
+     * @param playerName
+     * @return la string Win for joueur
+     */
+    private String getWinStr(String playerName) {
+        return new StringBuilder(STR_WIN).append(playerName).toString();
+    }
+
+    /**
+     * Concatenation de score1 - score2
+     *
+     * @param score1
+     * @param score2
+     * @return score1 - score2
+     */
+    private String getScoreStr(String score1, String score2) {
+        return new StringBuilder(score1).append(STR_SCORE_SEP).append(score2).toString();
+    }
+
+    /**
+     * @param points : le nombre de points
+     * @return la STR correspondante
+     * @Throw RuntimeException si invoqué dans un mauvais cas de figure
+     */
+    private String getStrForPoints(int points) {
+        switch (points) {
+            case 0:
+                return STR_0;
+            case 1:
+                return STR_15;
+            case 2:
+                return STR_30;
+            case 3:
+                return STR_40;
+            default:
+                throw new RuntimeException("getStrFroPoints :: Not enough information to handle score " + points);
+        }
     }
 }
