5f05e8d39421dd9c3728727270c41c8a89bce80e
fberge <fberge@ippon.fr>
2020-06-03 14:22:27 +0200

    KATA2 : extraction methodes et correction conditions pour utilisation returns

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,6 +1,15 @@
 
 public class TennisGame2 implements TennisGame
 {
+    public static final String SCORE_STR_LOVE = "Love";
+    public static final String SCORE_STR_FIFTEEN = "Fifteen";
+    public static final String SCORE_STR_THIRTY = "Thirty";
+    public static final String SCORE_STR_DEUCE = "Deuce";
+    public static final String SCORE_STR_FORTY = "Forty";
+    public static final String SCORE_SEPARATOR = "-";
+    public static final String SCORE_STR_ALL = "All";
+    public static final String SCORE_STR_ADVANTAGE = "Advantage ";
+    public static final String SCORE_STR_WIN_FOR = "Win for ";
     public int P1point = 0;
     public int P2point = 0;
     
@@ -16,108 +25,116 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score = "";
-        if (P1point == P2point && P1point < 4)
+        if (P1point == P2point && P1point < 3)
         {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
+            return getScoreIfEqualityLessThan4(score);
         }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
-        
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
+        if (P1point==P2point && P1point>=3) {
+            return SCORE_STR_DEUCE;
         }
         
+        if (P1point > 0 && P1point < 4 && P2point==0)
+        {
+            return getScoreIfP1gt0andP2eq0();
+        }
+        if (P2point > 0 && P2point < 4 && P1point==0)
+        {
+            return getScoreIfP2gt0andP1eq0();
+        }
+
         if (P1point>P2point && P1point < 4)
         {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
+            return getScoreIfP1gtP2andP1lt4();
         }
         if (P2point>P1point && P2point < 4)
         {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
+            return getScoreIdP2gtP1andP2lt4();
+        }
+
+        if (P1point > P2point && P2point >= 3 && P1point - P2point < 2)
+        {
+            return SCORE_STR_ADVANTAGE + player1Name;
         }
         
-        if (P1point > P2point && P2point >= 3)
+        if (P2point > P1point && P1point >= 3 && P2point - P1point < 2)
         {
-            score = "Advantage player1";
-        }
-        
-        if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2";
+            return SCORE_STR_ADVANTAGE + player2Name;
         }
         
         if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
         {
-            score = "Win for player1";
+            return SCORE_STR_WIN_FOR + player1Name;
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2";
-        }
+        return SCORE_STR_WIN_FOR + player2Name;
+    }
+
+    private String getScoreIdP2gtP1andP2lt4() {
+        String score;
+        if (P2point==2)
+            P2res=SCORE_STR_THIRTY;
+        if (P2point==3)
+            P2res=SCORE_STR_FORTY;
+        if (P1point==1)
+            P1res=SCORE_STR_FIFTEEN;
+        if (P1point==2)
+            P1res=SCORE_STR_THIRTY;
+        score = P1res + SCORE_SEPARATOR + P2res;
         return score;
     }
-    
-    public void SetP1Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P1Score();
-        }
-            
+
+    private String getScoreIfP1gtP2andP1lt4() {
+        String score;
+        if (P1point==2)
+            P1res=SCORE_STR_THIRTY;
+        if (P1point==3)
+            P1res=SCORE_STR_FORTY;
+        if (P2point==1)
+            P2res=SCORE_STR_FIFTEEN;
+        if (P2point==2)
+            P2res=SCORE_STR_THIRTY;
+        score = P1res + SCORE_SEPARATOR + P2res;
+        return score;
     }
-    
-    public void SetP2Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P2Score();
-        }
-            
+
+    private String getScoreIfP2gt0andP1eq0() {
+        String score;
+        if (P2point==1)
+            P2res = SCORE_STR_FIFTEEN;
+        if (P2point==2)
+            P2res = SCORE_STR_THIRTY;
+        if (P2point==3)
+            P2res = SCORE_STR_FORTY;
+
+        P1res = SCORE_STR_LOVE;
+        score = P1res + SCORE_SEPARATOR + P2res;
+        return score;
     }
-    
+
+    private String getScoreIfP1gt0andP2eq0() {
+        String score;
+        if (P1point==1)
+            P1res = SCORE_STR_FIFTEEN;
+        if (P1point==2)
+            P1res = SCORE_STR_THIRTY;
+        if (P1point==3)
+            P1res = SCORE_STR_FORTY;
+
+        P2res = SCORE_STR_LOVE;
+        score = P1res + "-" + P2res;
+        return score;
+    }
+
+    private String getScoreIfEqualityLessThan4(String score) {
+        if (P1point==0)
+            score = SCORE_STR_LOVE;
+        if (P1point==1)
+            score = SCORE_STR_FIFTEEN;
+        if (P1point==2)
+            score = SCORE_STR_THIRTY;
+        score += SCORE_SEPARATOR + SCORE_STR_ALL;
+        return score;
+    }
+
     public void P1Score(){
         P1point++;
     }
@@ -127,7 +144,7 @@ public class TennisGame2 implements TennisGame
     }
 
     public void wonPoint(String player) {
-        if (player == "player1")
+        if (player.equals(player1Name))
             P1Score();
         else
             P2Score();
